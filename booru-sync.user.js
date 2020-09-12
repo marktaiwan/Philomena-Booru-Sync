42c2c17c1aee369bc7e6bbc38502b0508fec24ce
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-09-12 10:08:22 +0800

    Version bump 1.3.1

--- booru-sync.user.js
+++ booru-sync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru Sync
 // @description  Sync faves and upvotes across boorus.
-// @version      1.3.0
+// @version      1.3.1
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
